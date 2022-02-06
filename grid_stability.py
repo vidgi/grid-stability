29a13c17b401cd05b0cb413e78455051999a6c61
vidgi <vzg805@gmail.com>
2022-02-05 20:43:46 -0600

    fixed typos

--- grid_stability.py
+++ grid_stability.py
@@ -52,7 +52,7 @@ def load_data():
 st.header('Background')
 st.subheader('Integrating Renewable Energy into the Electric Grid')
 
-st.write("As renewable energy sources are increasingly adopted, an infrastructure of new paradigms is necessary taking into account more production sources and management of a much more flexible and complex system to connect producers, conumers, and distribution.")
+st.write("As renewable energy sources are increasingly adopted, an infrastructure of new paradigms is necessary to account for more production sources and the management of a much more flexible and complex system that can connect producers, consumers, and distribution.")
 st.write("Traditional operating ecosystems previously involved fewer energy sources that supply energy to consumers over unidirectional flows. However, with more renewable options, the end users now can consume energy and also have the ability to produce and supply it, yielding the new classification of 'prosumers'. Now, the energy flow within distribution grids, or 'smart grids' is bidirectional.")
 st.write("This increased flexibility due to renewable sources and 'prosumers' has led to the management of supply and demand to be far more complex and challenging, leading to many studies looking into methods of predicting and managing smart grid stability.")
 
